79fd269ad72edadf55d93f58404a1976663e38f6
Samarthya-Gupta <50349628+Samarthya-Gupta@users.noreply.github.com>
2021-01-06 16:37:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+// const publicdir=path.join(__dirname,'../views')
+const app=express()
+
+// app.use(express.static(path.join(__dirname,'../views')))
+// app.use(express.static(publicdir))
+
+app.set('view engine', 'hbs')
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+// app.get('',(req,res)=>{
+//     res.send('hello express!')
+// })
+// app.get('/help',(req,res)=>{
+//     res.send('help page')
+// })
+const port = process.env.PORT || 80
+app.listen(port,()=>{
+    console.log('server is up on 3000')
+})
